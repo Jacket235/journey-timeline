11b87e4000cd500655d915b68781b600444c5aa3
Sebastian Mastalerz <sebastian.masti235@gmail.com>
2025-05-30 21:01:56 +0200

    asdsd

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,15 @@ app.use(express.json());
 app.post("/signup", (req, res) => {
     const { username, email, password } = req.body;
 
-    console.log("Username: " + username);
-    console.log("Email: " + email);
-    console.log("Password: " + password);
+    const query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)"
+    connection.query(query, [username, email, password], (err, result) => {
+        if (err) {
+            console.error("Error signing up", err);
+            return res.status(500).json({ error: "Database error" });
+        }
 
-    res.status(201).json({ message: "User registered", nick: username, mail: email, pass: password });
-    // const query = "INSERT INTO users (username, password) VALUES (?, ?, ?)"
-    // connection.query(query, [username, password]);
+        res.status(201).send("Registered");
+    });
 });
 
 const PORT = process.env.PORT || 8080;
